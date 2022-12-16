b92b6df2c003b479d03827818fd33ae4613b9c53
Shiwei Yu <shiwei1991@gmail.com>
2022-12-16 20:32:59 +0100

    reverse 0.0.6

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # reverse-proxy-golang
+
+## set up .env file with following configs
+
+    HOST="YOUR LOCAL HOST TO PROXY AT"
+
+## build for raspberry pi ARM v6 & v7
+
+    env GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -ldflags="-s -w" -o main-ARMv6 . && env GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -ldflags="-s -w" -o main-ARMv7 .
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("loading env failed")
+	}
+
+	host := os.Getenv("HOST")
+
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
-		Host:   "10.0.0.70:8081",
+		Host:   host,
 	})
 	http.ListenAndServe(":8081", proxy)
 }
